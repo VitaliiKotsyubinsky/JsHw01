e05dbb7a880aa45e89d459047e8de5211938d091
VitaliiKotsyubinsky <magnoom2010@gmail.com>
2021-11-30 22:21:01 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,12 @@
+let userName = prompt('Привіт! як тебе звати?');
+alert(`Привіт ${userName} !`);
+
+let userBirth = prompt('Коли ти народився? ^_^')
+const userAge = 2021 - userBirth;
+alert(`Вітаю, тобі ${userAge} !!!`);
+
+let question = prompt('А тепер назви мені будьяку довжину квадрату');
+const perimeterSquare = 4 * question;
+alert(`Периметр квадрата дорівнює ${perimeterSquare} `)
+
+
